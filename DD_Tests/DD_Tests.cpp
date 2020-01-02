9e2cdbfa67911be6c498663eaa4831399ab730b4
Igor Chesalin <chess777@inbox.lv>
2020-01-02 19:18:59 +0200

    Fix warnings

--- DD_Tests/DD_Tests.cpp
+++ DD_Tests/DD_Tests.cpp
@@ -5,23 +5,24 @@
 
 using namespace std;
 
-#define MAX_ARCH_NAME_SIZE  12
-#define CVT_BUF_SIZE        0x200ul
-#define ARHIVE_FILE         _T("c:\\Users\\Igor\\Documents\\Visual Studio 2013\\Projects\\DD Data\\DD\\SG.DL1")
+#define FILE_NAME_BUF_SIZE  (32768)
+#define ARHIVE_FILE         (_T("\\Documents\\Projects\\DD Data\\DD\\SG.DL1"))
+#define MAX_ARCH_NAME_SIZE  (12)
+#define CVT_BUF_SIZE        (0x200ul)
 
 #pragma pack(push, 1)
 
 struct ArhiveHdr{
-    BYTE    bSign[6];
-    WORD    wReserved1;
-    WORD    wEntryCount;
-    DWORD   dwFileTableOffset;
-    WORD    wReserved2;
+	BYTE    bSign[6];
+	WORD    wReserved1;
+	WORD    wEntryCount;
+	DWORD   dwFileTableOffset;
+	WORD    wReserved2;
 };
 
 struct ArhiveFileEntry{
-    char    pcFileName[MAX_ARCH_NAME_SIZE];
-    DWORD   dwFileSize;
+	char    pcFileName[MAX_ARCH_NAME_SIZE];
+	DWORD   dwFileSize;
 };
 
 #pragma pack(pop)
@@ -31,79 +32,87 @@ TCHAR   g_ptCvtString[CVT_BUF_SIZE];
 
 int ConvertMBCSToUni(const char *cpcMBString, wchar_t *pwcUString, int iUStringSize)
 {
-    return MultiByteToWideChar(932 /* Shift-JIS */, 0, cpcMBString, -1, pwcUString, iUStringSize);
+	return MultiByteToWideChar(932 /* Shift-JIS */, 0, cpcMBString, -1, pwcUString, iUStringSize);
 }
 
 int ConvertUniToMBCS(const wchar_t *cpwUString, char *pcMBString, int iMBStringSize)
 {
-    return WideCharToMultiByte(932 /* Shift-JIS */, 0, cpwUString, -1, pcMBString, iMBStringSize, NULL, NULL);
+	return WideCharToMultiByte(932 /* Shift-JIS */, 0, cpwUString, -1, pcMBString, iMBStringSize, NULL, NULL);
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-    HANDLE  hFile;
-    HANDLE  hFileMap;
-    void    *pvBuffer;
-    LARGE_INTEGER   ilFileSize;
-    DWORD   dwOffset;
-    DWORD   dwFileOffset;
-    bool    bError;
-    bool    bDone;
-    ArhiveHdr   sArhiveHeader;
-    //vector<ArhiveFileEntry>   vArhiveFileEntry;
-    ArhiveFileEntry sArhiveFileEntry;
-    int iIdx;
+	TCHAR   pcFileName[FILE_NAME_BUF_SIZE];
+	size_t  sFileNameSize = 0;
+	HANDLE  hFile;
+	HANDLE  hFileMap;
+	void    *pvBuffer;
+	LARGE_INTEGER   ilFileSize;
+	DWORD   dwOffset;
+	DWORD   dwFileOffset;
+	ArhiveHdr   sArhiveHeader;
+	//vector<ArhiveFileEntry>   vArhiveFileEntry;
+	ArhiveFileEntry sArhiveFileEntry;
+	int iIdx;
 
-    hFile = CreateFile(ARHIVE_FILE, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-    if (hFile == INVALID_HANDLE_VALUE){
-        _tprintf(_T("Can't open script file: %s\n"), ARHIVE_FILE);
-        return -2;
-    }
+	sFileNameSize = GetEnvironmentVariable(_T("USERPROFILE"), pcFileName, FILE_NAME_BUF_SIZE);
+	if (sFileNameSize == 0) {
+		_tprintf(_T("User profile not found\n"));
+		return -1;
+	}
 
-    hFileMap = CreateFileMapping(hFile, NULL, PAGE_READONLY, 0, 0, NULL);
-    if (hFileMap == INVALID_HANDLE_VALUE){
-        _tprintf(_T("Couldn't create file mapping: %s"), ARHIVE_FILE);
-        CloseHandle(hFile);
-        return -3;
-    }
+	_tcscat_s(pcFileName, ARHIVE_FILE);
 
-    pvBuffer = MapViewOfFile(hFileMap, FILE_MAP_READ, 0, 0, 0);
-    if (pvBuffer == NULL){
-        _tprintf(_T("Couldn't map view of file: %s"), ARHIVE_FILE);
-        CloseHandle(hFileMap);
-        CloseHandle(hFile);
-        return -4;
-    }
+	hFile = CreateFile(pcFileName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hFile == INVALID_HANDLE_VALUE){
+		_tprintf(_T("Can't open script file: %s\n"), pcFileName);
+		return -2;
+	}
 
-    GetFileSizeEx(hFile, &ilFileSize);
+	hFileMap = CreateFileMapping(hFile, NULL, PAGE_READONLY, 0, 0, NULL);
+	if (hFileMap == NULL){
+		_tprintf(_T("Couldn't create file mapping: %s"), pcFileName);
+		CloseHandle(hFile);
+		return -3;
+	}
 
-    memcpy(&sArhiveHeader, pvBuffer, sizeof(ArhiveHdr));
+	pvBuffer = MapViewOfFile(hFileMap, FILE_MAP_READ, 0, 0, 0);
+	if (pvBuffer == NULL){
+		_tprintf(_T("Couldn't map view of file: %s"), pcFileName);
+		CloseHandle(hFileMap);
+		CloseHandle(hFile);
+		return -4;
+	}
 
-    if (sArhiveHeader.bSign[0] != 'D' || sArhiveHeader.bSign[1] != 'L' || sArhiveHeader.bSign[2] != '1'
-        || (sArhiveHeader.dwFileTableOffset + sArhiveHeader.wEntryCount * sizeof(ArhiveFileEntry)) > ilFileSize.LowPart){
+	GetFileSizeEx(hFile, &ilFileSize);
 
-        _tprintf(_T("This is not a valid file: %s"), ARHIVE_FILE);
-        UnmapViewOfFile(pvBuffer);
-        CloseHandle(hFileMap);
-        CloseHandle(hFile);
-        return -5;
-    }
+	memcpy(&sArhiveHeader, pvBuffer, sizeof(ArhiveHdr));
 
-    dwOffset = sArhiveHeader.dwFileTableOffset;
-    dwFileOffset = sizeof(ArhiveHdr);
-    for (iIdx = 0; iIdx < sArhiveHeader.wEntryCount && dwOffset < ilFileSize.LowPart; iIdx++){
-        memcpy(&sArhiveFileEntry, (BYTE*)pvBuffer + dwOffset, sizeof(ArhiveFileEntry));
-        dwOffset += sizeof(ArhiveFileEntry);
-        strncpy_s(g_pcCvtString, CVT_BUF_SIZE, sArhiveFileEntry.pcFileName, MAX_ARCH_NAME_SIZE);
-        ConvertMBCSToUni(g_pcCvtString, g_ptCvtString, CVT_BUF_SIZE);
-        _tprintf(_T("%06d\t%s\t%ud\t%ud\n"), iIdx, g_ptCvtString, sArhiveFileEntry.dwFileSize, dwFileOffset);
-        dwFileOffset += sArhiveFileEntry.dwFileSize;
-    }
+	if (sArhiveHeader.bSign[0] != 'D' || sArhiveHeader.bSign[1] != 'L' || sArhiveHeader.bSign[2] != '1'
+		|| (sArhiveHeader.dwFileTableOffset + sArhiveHeader.wEntryCount * sizeof(ArhiveFileEntry)) > ilFileSize.LowPart){
 
-    UnmapViewOfFile(pvBuffer);
-    CloseHandle(hFileMap);
-    CloseHandle(hFile);
+		_tprintf(_T("This is not a valid file: %s"), pcFileName);
+		UnmapViewOfFile(pvBuffer);
+		CloseHandle(hFileMap);
+		CloseHandle(hFile);
+		return -5;
+	}
 
-    return 0;
+	dwOffset = sArhiveHeader.dwFileTableOffset;
+	dwFileOffset = sizeof(ArhiveHdr);
+	for (iIdx = 0; iIdx < sArhiveHeader.wEntryCount && dwOffset < ilFileSize.LowPart; iIdx++){
+		memcpy(&sArhiveFileEntry, (BYTE*)pvBuffer + dwOffset, sizeof(ArhiveFileEntry));
+		dwOffset += sizeof(ArhiveFileEntry);
+		strncpy_s(g_pcCvtString, CVT_BUF_SIZE, sArhiveFileEntry.pcFileName, MAX_ARCH_NAME_SIZE);
+		ConvertMBCSToUni(g_pcCvtString, g_ptCvtString, CVT_BUF_SIZE);
+		_tprintf(_T("%06d\t%s\t%ud\t%ud\n"), iIdx, g_ptCvtString, sArhiveFileEntry.dwFileSize, dwFileOffset);
+		dwFileOffset += sArhiveFileEntry.dwFileSize;
+	}
+
+	UnmapViewOfFile(pvBuffer);
+	CloseHandle(hFileMap);
+	CloseHandle(hFile);
+
+	return 0;
 }
 
